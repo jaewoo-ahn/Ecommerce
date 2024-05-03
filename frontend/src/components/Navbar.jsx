7b02f58d199551d7cbcdb1e8a3245e093e5e6438
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-05-03 21:55:00 +0900

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,37 @@
+import { NavLink } from "react-router-dom";
+import { MdHomeFilled, MdCategory, MdShop2, MdContacts } from "react-icons/md";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav
+    // className={`${containerStyles}`}
+    >
+      <NavLink to="/">
+        <div className="flexCenter gap-x-1">
+          <MdHomeFilled />
+          Home
+        </div>
+      </NavLink>
+      <NavLink to="/mens">
+        <div className="flexCenter gap-x-1">
+          <MdCategory />
+          Mens
+        </div>
+      </NavLink>
+      <NavLink to="/womens">
+        <div className="flexCenter gap-x-1">
+          <MdShop2 />
+          Womens
+        </div>
+      </NavLink>
+      <NavLink to="/kids">
+        <div className="flexCenter gap-x-1">
+          <MdContacts />
+          Kids
+        </div>
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Navbar;
