f87033cc908804f5f363111691f53b769892aeac
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-05-04 19:21:10 +0900

    pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,6 +13,14 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/" element={<Category />} />
+          <Route path="/" element={<Category />} />
+          <Route path="/" element={<Category />} />
+          <Route path="/" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/" element={<Cart />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </main>
