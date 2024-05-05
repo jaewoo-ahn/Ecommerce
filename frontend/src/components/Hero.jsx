1d46d6e771d5ef745aa76aa8403276199e78664f
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-05-05 23:43:19 +0900

    hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,5 +1,29 @@
+import { MdStar } from "react-icons/md";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section>
+      <div>
+        <h1>Digital Shopping Hub juction</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesqu. Donec pede justo, fringilla vel,
+          aliquet nec, vulputate eget, arcu
+        </p>
+        <div>
+          <div>
+            <MdStar />
+            <MdStar />
+            <MdStar />
+            <MdStar />
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
